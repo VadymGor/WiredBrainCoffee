a4b626a9b0121588a4f5bc6d492ee74eff56353e
VadymGor <07tennis@gmail.com>
2020-04-19 19:49:41 +0200

    Horizontally Scrolling Collection Views

--- WiredBrainCoffee/Scenes/GiftViewController/GiftViewController.swift
+++ WiredBrainCoffee/Scenes/GiftViewController/GiftViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class GiftViewController: UIViewController {
     
+    @IBOutlet weak var seasonalHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var seasonalCollectionView: UICollectionView!
     
     var seasonalGiftCards = [GiftCardModel]()
@@ -25,6 +26,18 @@ class GiftViewController: UIViewController {
             self.seasonalGiftCards = cards
             self.seasonalCollectionView.reloadData()
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        setHeightOfCollectionView()
+    }
+    
+    func setHeightOfCollectionView() {
+        let width = seasonalCollectionView.bounds.width - 30
+        let height = width / 1.5
+        seasonalHeightConstraint.constant = height
     }
 }
 
@@ -42,27 +55,9 @@ extension GiftViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let columns: CGFloat = 2
-        
-        // Get the width of the collection view
-        let collectionViewWidth = collectionView.bounds.width
-        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-        // Get the value for the space between the cells
-        let spaceBetweenCells = flowLayout.minimumInteritemSpacing * (columns - 1)
-        let sectionInsets = flowLayout.sectionInset.left + flowLayout.sectionInset.right
-        // Subtract the cells spacing from the collection view's width
-        let adjustedWidth = collectionViewWidth - spaceBetweenCells - sectionInsets
-        // Divide the adjusted width by columns
-        // floor: Rounds a number down to the nearest hole number
-        let width: CGFloat = floor (adjustedWidth / columns)
-        let height: CGFloat = width / 1.5
+        let width = seasonalCollectionView.bounds.width - 50
+        let height = width / 1.5
         return CGSize(width: width, height: height)
     }
     
-    // Sections Header
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "sectionHeader", for: indexPath)
-        return view
-    }
 }
